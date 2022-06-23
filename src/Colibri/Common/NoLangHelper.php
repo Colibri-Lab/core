4397bd8b9c4b66044df7732a0a6940c639b3f380
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-06-23 07:41:46 +0000

    -

---
+++ src/Colibri/Common/NoLangHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Helpers
+ *
+ * @author Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
+ * @copyright 2019 Colibri
+ * @package Colibri\Common
+ */
+
+namespace Colibri\Common;
+
+class NoLangHelper
+{
+    
+    public static function ParseString(string $value): string
+    {
+        $res = preg_match_all('/#\{(.*?)\}/i', $value, $matches, PREG_SET_ORDER);
+        if($res > 0) {
+            foreach($matches as $match) {
+                $parts = explode(';', $match[1]);
+                $lang = $parts[0];
+                $default = $parts[1] ?? '';
+                $replaceWith = $default;
+                $value = str_replace($match[0], str_replace('"', '&quot;', str_replace('\'', '`', $replaceWith)), $value);
+            }
+        }
+        return $value;
+    }
+
+    public static function ParseArray(array|object $array): array
+    {
+        $ret = [];
+        foreach($array as $key => $value) {
+            if(is_array($value)) {
+                $ret[$key] = self::ParseArray($value);
+            }
+            else if(is_object($value)) {
+                if(method_exists($value, 'ToArray')) {
+                    $value = $value->ToArray();
+                }
+                $ret[$key] = self::ParseArray($value);
+            }
+            else {
+                if(is_string($value)) {
+                    $value = self::ParseString($value);
+                }
+                $ret[$key] = $value;
+            }
+        }
+        return $ret;
+    }
+}
