989460b8a74a662592eb90129c69e30757b255ac
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-08-06 14:22:27 +0300

    -

--- src/Colibri/Web/Response.php
+++ src/Colibri/Web/Response.php
@@ -420,6 +420,9 @@ class Response
 
             foreach($cookies as $cookie) {
                 // (object)['name' => 'ss-jwt', 'value' => $session->jwt, 'expire' => time() + 365 * 86400, 'domain' => Request::$i->server->host, 'path' => '/', 'secure' => true]
+                if(!isset($cookie->name)) {
+                    continue;
+                }
                 setcookie($cookie->name, $cookie->value, [
                     'expires' => $cookie->expire ?? 0, 
                     'path' => $cookie->path ?? '', 
