b472f586b25e9f3a351bdf2a046cbc1ac5da075b
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-12-13 22:25:14 +0300

    -

---
+++ src/Colibri/Utils/Minifiers/Javascript.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Colibri\Utils\Minifiers;
+use Colibri\App;
+use Colibri\IO\FileSystem\File;
+
+
+class Javascript 
+{
+
+    private ?object $_config = null;
+
+    public static function Minify(string $content): string
+    {
+        $self = new self();
+        return $self->Run($content);
+    }
+
+    public function __construct()
+    {
+        $this->_config = App::$config->Query('minifier')->AsObject();
+    }
+
+    public function Run(string $content): string
+    {
+        if($this->_config?->type === 'uglify') {
+
+            $commandline = $this->_config->command;
+            $time = microtime(true);
+            $cacheFileIn = App::$webRoot . App::$config->Query('runtime')->GetValue() . 'code'.$time.'.js';
+            $cacheFileOut = App::$webRoot . App::$config->Query('runtime')->GetValue() . 'code'.$time.'-minified.js';
+            
+            File::Write($cacheFileIn, $content);
+            $commandline = sprintf($commandline, $cacheFileIn, $cacheFileOut);
+            shell_exec($commandline);
+            $content = File::Read($cacheFileOut);
+
+            File::Delete($cacheFileIn);
+            File::Delete($cacheFileOut);
+
+        } elseif ($this->_config?->type === 'shrink') {
+            return \JShrink\Minifier::minify($content, array('flaggedComments' => false));
+        }
+
+        return $content;
+
+    }
+
+}
