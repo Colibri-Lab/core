ccf38ae24664e25ef02bb09df73b2446cd840125
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-08-03 19:46:09 +0300

    -

--- src/Colibri/Exceptions/PermissionDeniedException.php
+++ src/Colibri/Exceptions/PermissionDeniedException.php
@@ -4,4 +4,6 @@ namespace Colibri\Exceptions;
 
 class PermissionDeniedException extends \Exception {
 
+    public const PermissionDeniedMessage = 'Permission denied'; 
+
 }
--- src/Colibri/Web/Controller.php
+++ src/Colibri/Web/Controller.php
@@ -2,12 +2,10 @@
 
 /**
  * Kласс обработчика Controller
- * 
  * @author Ваган Григорян <vahan.grigoryan@gmail.com>
  * @copyright 2019 Colibri
  * @package Colibri\Web
  * @version 1.0.0
- * 
  */
 
 namespace Colibri\Web;
@@ -16,66 +14,64 @@ use Colibri\Common\StringHelper;
 
 /**
  * Абстрактный класс для обработки Web запросов
- * 
+ *
  * Наследуемся от него и создаем функцию, которую можно будет вызвать
- * например: 
- * 
- * запрос: 
+ * например:
+ *
+ * запрос:
  * /buh/web/page/method1.html
  * /buh/web/page/method1.json
  * /buh/web/page/method1.xml
- * 
+ *
  * namespace App\Controllers\Buh\Web
- * 
+ *
  * class PageController extends Colibri\Web\Controller {
- * 
+ *
  *      public function Method1($get, $post, $payload) {
- *          
+ *
  *          тут пишем что нужно и финишируем функцией Finish
- * 
+ *
  *          внимание:
  *          $get, $post, $payload - изменять бессмысленно, так как это копии данных переданных в запросе
- *          
+ *
  *          ЗАПРЕЩАЕТСЯ:
  *          1. выводить что либо с помощью функции echo, print_r, var_dump и т.д.
  *          2. запрашивать другие RPC Handler-ы на том же уровне
- *          3. реализовывать бизнес-логику в классе-обработчике (наследники RpcHandler) 
- *          
+ *          3. реализовывать бизнес-логику в классе-обработчике (наследники RpcHandler)
+ *
  *          $code = 200 | 400 и т.д.
  *          $message = какое либо сообщение
- *          $result = peyload ответа, может быть строкой в случае с html/xml 
- * 
+ *          $result = peyload ответа, может быть строкой в случае с html/xml
+ *
  *          ! НИКАКОГО ECHO !!!! ЗАПРЕЩЕНО
- * 
+ *
  *          пример результата:
- * 
+ *
  *          div => [
  *              span => тестт
  *          ]
- * 
+ *
  *          xml хелпер создаст:
- * 
+ *
  *          <div><span>тестт</span></div>
- *          
+ *
  *          html хелпер создаст:
- * 
+ *
  *          <div class="div"><div class="span">тестт</div></div>
- * 
- *  
- *          return $this->Finish(int $code, string $message, mixed $result);         
- * 
+ *
+ *
+ *          return $this->Finish(int $code, string $message, mixed $result);
+ *
  *      }
- * 
- * 
+ *
  * }
- * 
+ *
  */
 class Controller
 {
+    protected ?string $_type = null;
 
-    protected ?string $_type = null; 
-
-    public function __construct(?string $type = null) 
+    public function __construct(?string $type = null)
     {
         $this->_type = $type;
     }
@@ -91,8 +87,14 @@ class Controller
      * @return \stdClass готовый результат
      * @testFunction testFinish
      */
-    public function Finish(int $code, string $message, mixed $result = null, string $charset = 'utf-8', array $headers = [], array $cookies = []): object
-    {
+    public function Finish(
+        int $code,
+        string $message,
+        mixed $result = null,
+        string $charset = 'utf-8',
+        array $headers = [],
+        array $cookies = []
+    ): object {
         $res = (object) [];
         $res->code = $code;
         $res->message = $message;
@@ -106,7 +108,7 @@ class Controller
     /**
      * Создаем ссылку для добавления в url
      *
-     * @param string $method название функции в классе контроллера 
+     * @param string $method название функции в классе контроллера
      * @param string $type тип возвращаемого значения: json, xml, html
      * @param array $params GET параметры
      * @return string
