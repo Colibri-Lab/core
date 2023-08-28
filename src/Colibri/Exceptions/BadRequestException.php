d2f56b5b601821c9041034a26a0623a957eb17f0
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-08-28 12:36:57 +0300

    -

--- src/Colibri/Exceptions/BadRequestException.php
+++ src/Colibri/Exceptions/BadRequestException.php
@@ -4,5 +4,8 @@ namespace Colibri\Exceptions;
 
 class BadRequestException extends \Exception
 {
+
+    public const BadRequestExceptionCode = 400;
+
     public const BadRequestExceptionMessage = 'Bad request';
 }
