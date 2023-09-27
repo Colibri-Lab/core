f2441a5f6f6f2fb27f54c74be7e5c546ed9b1369
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-09-27 10:31:17 +0300

    -

--- src/Colibri/Exceptions/PermissionDeniedException.php
+++ src/Colibri/Exceptions/PermissionDeniedException.php
@@ -4,6 +4,7 @@ namespace Colibri\Exceptions;
 
 class PermissionDeniedException extends \Exception
 {
+    public const PermissionDeniedCode = 403;
     public const PermissionDeniedMessage = 'Permission denied';
 
 }
