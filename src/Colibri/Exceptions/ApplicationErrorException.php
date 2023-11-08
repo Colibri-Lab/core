1aaccaa7a04b5ca277806b37e369b8ddf09cc672
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-11-08 08:23:00 +0000

    -

--- src/Colibri/Exceptions/ApplicationErrorException.php
+++ src/Colibri/Exceptions/ApplicationErrorException.php
@@ -4,6 +4,9 @@ namespace Colibri\Exceptions;
 
 class ApplicationErrorException extends \Exception
 {
+
+    public const ErrorCode = 500;
+
     public const ValidationError = 'Application validation error';
     public const ApplicationError = 'Application Error';
 }
